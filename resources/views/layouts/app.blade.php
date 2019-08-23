e795afbc9de85e72d0529e36be7b70ba44401cd3
EC2 Default User <ec2-user@ip-172-31-92-219.ec2.internal>
2019-08-23 12:32:20 +0000

    third commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
 <head>
 <title>user register</title>
 <!-- CSS と JavaScript -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <script src="https://d.line-scdn.net/liff/1.0/sdk.js"></script>
 <script type="text/javascript">
@@ -33,22 +34,22 @@ window.onload = function (e) {
 };
 
 // プロファイルの取得と表示
-function getProfile(){
-    // https://developers.line.me/ja/reference/liff/#liffgetprofile()
-    liff.getProfile().then(function (profile) {
-        document.getElementById('').textContent = profile.userId;
-        document.getElementById('').textContent = profile.displayName;
+// function getProfile(){
+//     // https://developers.line.me/ja/reference/liff/#liffgetprofile()
+//     liff.getProfile().then(function (profile) {
+//         document.getElementById('').textContent = profile.userId;
+//         document.getElementById('').textContent = profile.displayName;
 
-        var profilePictureDiv = document.getElementById('');
-        if (profilePictureDiv.firstElementChild) {
-            profilePictureDiv.removeChild(profilePictureDiv.firstElementChild);
-        }
+//         var profilePictureDiv = document.getElementById('');
+//         if (profilePictureDiv.firstElementChild) {
+//             profilePictureDiv.removeChild(profilePictureDiv.firstElementChild);
+//         }
 
-        document.getElementById('statusmessagefield').textContent = profile.statusMessage;
-    }).catch(function (error) {
-        window.alert("Error getting profile: " + error);
-    });
-}
+//         document.getElementById('statusmessagefield').textContent = profile.statusMessage;
+//     }).catch(function (error) {
+//         window.alert("Error getting profile: " + error);
+//     });
+// }
 
 </script>
 
