a91d6205f6ffa1d890952a9fab1e2e08b80eb4f2
EC2 Default User <ec2-user@ip-172-31-92-219.ec2.internal>
2019-08-23 13:17:25 +0000

    third commit

--- resources/views/userregister.blade.php
+++ resources/views/userregister.blade.php
@@ -5,8 +5,8 @@
     <!-- Bootstrapの定形コード… -->
     <div class="card-body">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.css">
-        
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css"/>
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css"/>
         <div class="card-title">
             店舗情報登録
         </div>
@@ -238,7 +238,7 @@
     
     </div>
     
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js"></script>
     <script type="text/javascript">
         $(document).ready(function(){
             $('.your-slide-class').slick({
